585ff5682986d405381623f61cdba1248d7481ea
Karoly Kozma <karoly.kozma@sap.com>
2019-09-14 21:55:30 +0000

    Add a List Control to the View

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,23 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function (Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageBox"
+], function (Controller, MessageBox) {
 	"use strict";
 
 	return Controller.extend("sapneo.my.wire.wire_ui5_neo.controller.View1", {
 		onInit: function () {
 
+		},
+		
+		// show in a pop-up which list element was pressed
+		handleListItemPress: function (oEvent) {
+			MessageBox.show(
+				"You pressed item: " + oEvent.getSource().getBindingContext(), {
+					icon: sap.m.MessageBox.Icon.INFORMATION,
+					title: "It works!",
+					actions: [sap.m.MessageBox.Action.OK]
+				}
+			);
 		}
 	});
 });
